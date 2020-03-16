706912d7cf62429d7ef3efb3115155b02f61fc8e
Fabio Julio <fabioccomp@gmail.com>
2020-03-16 12:58:53 -0300

    aula2

--- App.js
+++ App.js
@@ -3,17 +3,55 @@ import React, {Fragment} from 'react';
 import {
   Text,
   StatusBar,
+  Dimensions,
+  StyleSheet,
+  ScrollView,
+  FlatList,
+  View,
+  Image,
 } from 'react-native';
 
+const informacoes = [
+  {
+    id: 1,
+    usuario: 'Fabio'
+  },
+  {
+    id: 2,
+    usuario: 'João'
+  },
+  {
+    id: 3,
+    usuario: 'José'
+  },
+]
+
 const App = () => {
+  console.log(informacoes);
   return (
-    <Fragment>
-      <StatusBar barStyle="dark-content" />
-      <Text>Hello World!!</Text>
-      <Text>Hello World!!</Text>
-      <Text>Hello World!!</Text>
-    </Fragment>
+      <FlatList
+        data={informacoes}
+        renderItem={({item}) =>
+          <Fragment>
+            <Text>{item.usuario}</Text>
+            <Image 
+              source={require('./res/img/alura.png')}
+              style={estilo.imagem}
+            />
+          </Fragment>  
+        }
+        keyExtractor={item => item.id.toString()}
+      />
   );
 };
+
+const largura = Dimensions.get("screen").width;
+
+const estilo = StyleSheet.create({
+  imagem: {
+    width: largura,
+    height: largura
+  }
+})
 
 export default App;

605b7c10b0b7fd601faea0c5b5fa63c672969fe2
Fabio Julio <fabioccomp@gmail.com>
2020-03-16 10:44:49 -0300

    primeiro commit

---
+++ App.js
@@ -0,0 +1,19 @@
+
+import React, {Fragment} from 'react';
+import {
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <Fragment>
+      <StatusBar barStyle="dark-content" />
+      <Text>Hello World!!</Text>
+      <Text>Hello World!!</Text>
+      <Text>Hello World!!</Text>
+    </Fragment>
+  );
+};
+
+export default App;
